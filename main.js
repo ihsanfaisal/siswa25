3d42ce548e19bbcf344ea521730f6bd04062990c
ihsanfaisal <ihsanfaisal@gmail.com>
2024-08-12 10:29:42 +0700

    menambah konfigurasi firebase

---
+++ main.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
+  authDomain: "insan-cemerlang.firebaseapp.com",
+  projectId: "insan-cemerlang",
+  storageBucket: "insan-cemerlang.appspot.com",
+  messagingSenderId: "579109661574",
+  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
+};
