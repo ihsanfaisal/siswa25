66baccd7c025e4ead220890c28dbde1ed1b6dcf3
ihsanfaisal <ihsanfaisal@gmail.com>
2024-08-12 11:03:09 +0700

    menginisialisasi firebase

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ import {
   orderBy
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 
+// ini adalah konfigurasi firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
   authDomain: "insan-cemerlang.firebaseapp.com",
@@ -20,3 +21,7 @@ const firebaseConfig = {
   messagingSenderId: "579109661574",
   appId: "1:579109661574:web:4a7cd4060f70eded945a07"
 };
+
+// Inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
